6f491fde187bb47cfdd12c5062c2b0609a9884b1
abdoola1 <abdoola1@hawkmail.newpaltz.edu>
2019-11-03 18:16:51 -0300

    hopeful

--- client/src/models/Game.js
+++ client/src/models/Game.js
@@ -1,7 +1,16 @@
 export const Game_Server = {
     Players: [],
     Picture_Deck: [],
-    Caption_Deck: [],
+    Caption_Deck: [
+        {quote:"what do you meme"},
+        {quote:"so it has come to this"},
+        {quote:"i will slit your throat with a rusty turtle"},
+        {quote:"i got some damsels to go distress"},
+        {quote:"hang in there baby"},
+        {quote:"your father is not a glass maker"},
+        {quote:"quote"},
+
+    ],
     Dealer: -1,
     Captions_In_Play: [], // strings
     Picture_In_Play: "",
@@ -21,7 +30,9 @@ export const Game_Client = {
     Caption_Chosen: -1
 }
 
-export var My_Captions = [];
+export var My_Captions = {
+
+}
 
 export class Player {
     name;
--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -20,20 +20,53 @@
             </ul>
 
         </div>
-        <div class="column">
-            Second column
+        <div class="column is-one-quarter">
+            
+            <ul class="panel">
+                <p class="panel-heading">
+                    Quotes
+                </p>
+                <li v-for="(p, i) in server.Caption_Deck " :key="i" class="panel-block is-active">
+                    <span class="panel-icon">
+                    <i class="fa fa-comments" aria-hidden="true"></i>
+                    </span>
+                    {{p.quote}}
+                </li>
+            </ul>
+
         </div>
     </div>
+    <div class="columns">
+        <div class="column is-one-quarter">
+            
+            <ul class="panel">
+                <p class="panel-heading">
+                    Quotes maybe placed to side to allow for game screen
+                </p>
+                <li v-for="(p, i) in server.Caption_Deck " :key="i" class="panel-block is-active">
+                    <span class="panel-icon">
+                    <i class="fa fa-comments" aria-hidden="true"></i>
+                    </span>
+                    {{p.quote}}
+                </li>
+            </ul>
 
+        </div>
+        <div class="column">
+            3rd column
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
-import { Game_Client, My_Captions } from "../models/Game";
+import { Game_Client, Game_Server } from "../models/Game";
 
 export default {
     data: ()=> ({
-        game: Game_Client
+        game: Game_Client,
+        server: Game_Server
+
     })
 }
 </script>
